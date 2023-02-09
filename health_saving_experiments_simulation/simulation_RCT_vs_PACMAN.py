a8ec2ab10ade4f05e76a73d60a8f5f81abac8263
Sree Ayyar <s.ayyar1@lse.ac.uk>
2023-02-09 14:26:32 +0000

    checks/questions on code for 10/02

--- health_saving_experiments_simulation/simulation_RCT_vs_PACMAN.py
+++ health_saving_experiments_simulation/simulation_RCT_vs_PACMAN.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import random
 
-data = pd.read_csv('health_saving_experiments_simulation\HARP_ROSCA_data.csv')
+data = pd.read_csv('/Users/sreeayyar/Dropbox/GitHub/PACMAN/health_saving_experiments_simulation/HARP_ROSCA_data.csv')
 
 """
 the 4 treatments are:
@@ -21,7 +22,7 @@ the outcome is:
 data['multitreat'].value_counts()
 
 # drop the users that multitreated
-# TODO Do we really want to throw away all the data from these users?
+# TODO Do we really want to throw away all the data from these users? - I think so; it is just over 10% of the sample..
 data = data[data['multitreat'] == 0]
 
 # how many people in each treatment?
@@ -81,17 +82,24 @@ print('ATE_health_pot: ', ATE_health_pot)
 print('ATE_health_savings: ', ATE_health_savings)
 
 # the best one is health_savings
-# (also, the only one in the paper significant at 5%)
+# TODO: also, the only one in the paper significant at 5% - 
+    # no. which table are you looking at? imo reference should be Table 3, columns (3) and (4)
+    # should we be replicating their results for credibility? what about CIA-based arguments?
 
 epsilon = 0
 n_people = 125   # simulate 2 RCTs with n_people each (+ control group)
 n_simulations = 1000
 success_count = 0
+randomtreat = int(1) # set to 1 if you want horserace to be against random two RCT treatments
 
 for _ in range(n_simulations):
     # select the 2 treatments randomly
-    T1, T2 = np.random.choice(
-        ['safe_box', 'locked_box', 'health_pot', 'health_savings'], 2, replace=True)
+    if randomtreat == 1:
+        T1, T2 = np.random.choice(
+            ['safe_box', 'locked_box', 'health_pot', 'health_savings'], 2, replace=True)
+    if randomtreat == 0:
+        T1 = 'health_pot'
+        T2 = 'safe_box'
     avg_Y_control = data['fol2_illness_untreated_3mo'][data['encouragement'] == 1].sample(
         n_people, replace=True).mean()  # TODO should we ignore control?
     avg_Y_T1 = data['fol2_illness_untreated_3mo'][data[T1]
@@ -104,7 +112,7 @@ for _ in range(n_simulations):
     ATE_T2 = avg_Y_T2 - avg_Y_control
 
     # did we find the best treatment?
-    # TODO if you have epsilon > 0, change the condition for success
+    # TODO if you have epsilon > 0, change the condition for success 
     assert epsilon == 0
     if T1 == best_treatment:
         if ATE_T1 > ATE_T2:
@@ -144,12 +152,12 @@ def probability_eps_optimal(alpha,
     # create a pandas dataframe of the samples
     thetas = pd.DataFrame()
     for treatment in list_treatments:
-        # sample 1000 thetas from the beta distribution
+        # sample 10000 thetas from the beta distribution
         thetas[treatment] = np.random.beta(
-            alpha[treatment], beta[treatment], 10000)
+            alpha[treatment], beta[treatment], 10000) 
 
     # calculate the probability of being an eps-optimal treatment,
-    # that is, what is the probability that theta is the highest among all thetas or epsilon close to the highest
+    # that is, what is the probability that theta is the highest among all thetas, or epsilon close to the highest
 
     # for each treatment, calculate the probability of being an eps-optimal treatment
     prob_eps_optimal = {}
@@ -159,7 +167,7 @@ def probability_eps_optimal(alpha,
             thetas[treatment] >= thetas.max(axis=1) - epsilon).mean() # TODO this does not seem to do what you think it does!!
     return prob_eps_optimal
 
-n_simulations = 100
+n_simulations = 1000 
 success_count = 0
 for sim in range(n_simulations):
 
@@ -181,8 +189,6 @@ for sim in range(n_simulations):
                                                                 # 0.27 (second best)
                                                                 # 0.32 (BEST)
                                                                 # 0.19
-
-
     total_people_used = 0
     # round one:
     for treatment in ['safe_box', 'locked_box', 'health_pot', 'health_savings']:
@@ -199,7 +205,7 @@ for sim in range(n_simulations):
 
     # choose the 2 treatments with the highest probability of being epsilon-optimal:
     # get the probabilities of being an eps-optimal treatment
-    # TODO is this a good rule? should I get rid off half the treatments? 
+    # TODO is this a good rule? should I get rid off half the treatments? - median elimination!
     prob_eps_optimal = probability_eps_optimal(dict_alpha, dict_beta, epsilon)
     # print(prob_eps_optimal)
 
@@ -209,7 +215,7 @@ for sim in range(n_simulations):
     if T1 != best_treatment and T2 != best_treatment:
         continue
 
-    # update the epsilon and the number of people to sample TODO 
+    # TODO: update the epsilon and the number of people to sample?
     # epsilon =  ?
     # n_people_multiarmed = ?
 
@@ -231,13 +237,37 @@ for sim in range(n_simulations):
         success_count += 1
     # print('\n')
     # print('alpha: ', dict_alpha)
-    # print('beta: ', dict_beta)
-
+    # print('beta: ', dict_beta)`
 
 print('success rate on multiarmed bandits: ', success_count/n_simulations)
 print('total people used: ', total_people_used)
 
+# TODO: do not randomly select RCT treatments, run it against the "best" ATE treatment/base it off paper hypothesis/horserace against any 2 treatments - 
+    # it seems this randomness is driving our bandits being better? - if economists know what to do, then all good?
+# TODO: try different epsilons (0.05 is high for a binary variable; let us go 0.01) - normative judgement (plot graph)
 
-# TODO IT SEEMS THAT THE FUNCTION probability_eps_optimal DOES NOT DO WHAT YOU THINK IT DOES.
-# TODO TEST IT! 
+# %% Tests of the probability_eps_optimal function:
+
+# TODO: IT SEEMS THAT THE FUNCTION probability_eps_optimal DOES NOT DO WHAT YOU THINK IT DOES - in what sense??
+
+random.seed(17)
+
+scores = pd.DataFrame()
+
+scores[1] = [97,77,99,77,17]
+scores[2] = [18,188,20,14,12]
+scores[3] = [89,55,12,59,23]
+
+prob_eps_optimal = (scores[1] >= scores.max(axis=1) - epsilon).mean()
+prob_eps_optimal
+
+scores = pd.DataFrame()
+
+scores[1] = np.random.beta(1, 1, 20) 
+scores[2] = np.random.beta(1, 1, 20) 
+scores[3] = np.random.beta(1, 1, 20) 
+scores
+
+prob_eps_optimal = (scores[1] >= scores.max(axis=1) - epsilon).mean()
+prob_eps_optimal
 
