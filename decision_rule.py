e44d6a318213b4aa8a62d1ff2c47b6d76e9aa08e
Sree Ayyar <s.ayyar1@lse.ac.uk>
2023-05-20 10:06:36 +0100

    update decision_rule.py

--- decision_rule.py
+++ decision_rule.py
@@ -3,6 +3,7 @@ from tqdm import trange
 import random
 import scipy.stats as stats
 import scipy.special as special
+import math
 
 # numerical approximation
 def numerical_integration(alpha, beta, epsilon, n_samples):
@@ -63,8 +64,41 @@ def scale_approx(alpha, beta, epsilon):
             alpha[j]=((epsilon-mu)*(epsilon*(1-epsilon)-epsilon*mu-(1-epsilon)*mu+mu**2+sigma2))/sigma2
             beta[j] = ((mu-1+epsilon)*(epsilon*(1-epsilon)-epsilon*mu-(1-epsilon)*mu+mu**2+sigma2))/sigma2
             numerator = 2*special.beta(alpha[i]+alpha[j],beta[i]+beta[j])
-            denominator = special.beta(alpha[i]-2,beta[i]-2)*special.beta(alpha[j]-2,beta[j]-2)*(alpha[i]-alpha[j])
-            prob_thetas[i,j] = numerator/denominator
+            if alpha[i] > alpha[j]:
+                denominator = special.beta(alpha[i]-2,beta[i]-2)*special.beta(alpha[j]-2,beta[j]-2)*(alpha[i]-alpha[j])
+                prob_thetas[i,j] = numerator/denominator
+            else:
+                denominator = special.beta(alpha[i]-2,beta[i]-2)*special.beta(alpha[j]-2,beta[j]-2)*(alpha[j]-alpha[i])
+                prob_thetas[i,j] = 1-(numerator/denominator)
+
+    pairwise_DR = np.zeros((n_treatments,n_treatments))
+    for i in range(n_treatments):
+        for j in range(n_treatments):
+            if prob_thetas[i,j] > prob_thetas[j,i]:
+                pairwise_DR[i,j] = 1
+
+    pr_epsilon_optimal = np.sum(pairwise_DR, axis=1)
+
+    sorted_indices = np.argsort(pr_epsilon_optimal)[::-1]
+    top_half_indices = sorted_indices[:n_treatments // 2]
+    bool_survived_treatments = np.zeros(n_treatments)
+    bool_survived_treatments[top_half_indices] = 1
+
+    return bool_survived_treatments
+
+def exact_sum(alpha, beta, epsilon):
+    
+    n_treatments = len(alpha)
+
+    prob_thetas = np.ones((n_treatments,n_treatments))
+    
+    for i in range(n_treatments):
+        for j in range(n_treatments):
+            total = 0
+            for k in range(alpha[i]):
+                total += math.exp(math.lbeta(alpha[j]+k,beta[i]+beta[j])
+                        - math.log(beta[i]+k) - math.lbeta(1+k, beta[i]) - math.lbeta(alpha[j], beta[j]))
+            prob_thetas[i,j] = total
 
     pairwise_DR = np.zeros((n_treatments,n_treatments))
     for i in range(n_treatments):
@@ -95,6 +129,7 @@ array_shape = (len(n_sample_range), len(epsilon_range), len(n_treatments_range))
 counter_ni = np.zeros(array_shape)
 counter_na = np.zeros(array_shape)
 counter_sa = np.zeros(array_shape)
+counter_es = np.zeros(array_shape)
 
 # numerical integration decision rule
 for sample in n_sample_range:
@@ -106,7 +141,9 @@ for sample in n_sample_range:
             theta = np.random.uniform(0,1,n_treatments)
             best_theta = np.argmax(theta)
             print("Best treatment is " + str(best_theta))
-            
+            epsilon_opt_thetas = np.where(np.logical_and(theta>=theta[best_theta]-epsilon, theta<=theta[best_theta]+epsilon))
+            print("Epsilon optimal treatments are " + str(epsilon_opt_thetas))
+
             # uniform prior
             alphas = np.ones(n_treatments)
             betas = np.ones(n_treatments)
@@ -114,7 +151,8 @@ for sample in n_sample_range:
             for i in trange(sims): 
                 remaining_treatments = np.arange(n_treatments)
                 j = 1
-                if j < 3:      
+                rounds = math.floor(math.log2(n_treatments))
+                if j < rounds:      
                     outcome_treatment = np.random.binomial(sample, theta[remaining_treatments])
                     alphas[remaining_treatments] = alphas[remaining_treatments] + outcome_treatment
                     betas[remaining_treatments] = betas[remaining_treatments] + sample - outcome_treatment
@@ -126,17 +164,34 @@ for sample in n_sample_range:
                     
                     remaining_treatments = remaining_treatments[bool_survived_treatments == 1]
                     j = j + 1
-                if np.any(remaining_treatments == best_theta)==1:
+                if np.in1d(remaining_treatments,epsilon_opt_thetas).any()==1:
                     pos1 = n_sample_range.index(sample)
                     pos2 = epsilon_range.index(epsilon)
                     pos3 = n_treatments_range.index(n_treatments)
                     counter_ni[pos1,pos2,pos3] = counter_ni[pos1,pos2,pos3] + 1
+                    
+# normal approx decision rule
+for sample in n_sample_range:
+    for epsilon in epsilon_range:
+        for n_treatments in n_treatments_range:
+            print(f"sample_size: {sample}, epsilon: {epsilon}, n_treatments: {n_treatments}")
             
-            # normal approx decision rule
+            # let us fix true probabilities
+            theta = np.random.uniform(0,1,n_treatments)
+            best_theta = np.argmax(theta)
+            print("Best treatment is " + str(best_theta))
+            epsilon_opt_thetas = np.where(np.logical_and(theta>=theta[best_theta]-epsilon, theta<=theta[best_theta]+epsilon))
+            print("Epsilon optimal treatments are " + str(epsilon_opt_thetas))
+            
+            # uniform prior
+            alphas = np.ones(n_treatments)
+            betas = np.ones(n_treatments)
+
             for i in trange(sims): 
                 remaining_treatments = np.arange(n_treatments)
                 j = 1
-                if j < 3:      
+                rounds = math.floor(math.log2(n_treatments))
+                if j < rounds:      
                     outcome_treatment = np.random.binomial(sample, theta[remaining_treatments])
                     alphas[remaining_treatments] = alphas[remaining_treatments] + outcome_treatment
                     betas[remaining_treatments] = betas[remaining_treatments] + sample - outcome_treatment
@@ -146,17 +201,35 @@ for sample in n_sample_range:
                                                             epsilon)
                     
                     remaining_treatments = remaining_treatments[bool_survived_treatments == 1]
-                if np.any(remaining_treatments == best_theta)==1:
+                if np.in1d(remaining_treatments,epsilon_opt_thetas).any()==1:
                     pos1 = n_sample_range.index(sample)
                     pos2 = epsilon_range.index(epsilon)
                     pos3 = n_treatments_range.index(n_treatments)
                     counter_na[pos1,pos2,pos3] = counter_na[pos1,pos2,pos3] + 1
+            
                                 
-            # scale approx decision rule
+# scale approx decision rule
+for sample in n_sample_range:
+    for epsilon in epsilon_range:
+        for n_treatments in n_treatments_range:
+            print(f"sample_size: {sample}, epsilon: {epsilon}, n_treatments: {n_treatments}")
+            
+            # let us fix true probabilities
+            theta = np.random.uniform(0,1,n_treatments)
+            best_theta = np.argmax(theta)
+            print("Best treatment is " + str(best_theta))
+            epsilon_opt_thetas = np.where(np.logical_and(theta>=theta[best_theta]-epsilon, theta<=theta[best_theta]+epsilon))
+            print("Epsilon optimal treatments are " + str(epsilon_opt_thetas))
+            
+            # uniform prior
+            alphas = np.ones(n_treatments)
+            betas = np.ones(n_treatments)
+
             for i in trange(sims): 
                 remaining_treatments = np.arange(n_treatments)
                 j = 1
-                if j < 3:      
+                rounds = math.floor(math.log2(n_treatments))
+                if j < rounds:      
                     outcome_treatment = np.random.binomial(sample, theta[remaining_treatments])
                     alphas[remaining_treatments] = alphas[remaining_treatments] + outcome_treatment
                     betas[remaining_treatments] = betas[remaining_treatments] + sample - outcome_treatment
@@ -166,10 +239,52 @@ for sample in n_sample_range:
                                                             epsilon)
                     
                     remaining_treatments = remaining_treatments[bool_survived_treatments == 1]
-                if np.any(remaining_treatments == best_theta)==1:
+                if np.in1d(remaining_treatments,epsilon_opt_thetas).any()==1:
                     pos1 = n_sample_range.index(sample)
                     pos2 = epsilon_range.index(epsilon)
                     pos3 = n_treatments_range.index(n_treatments)
                     counter_sa[pos1,pos2,pos3] = counter_sa[pos1,pos2,pos3] + 1
-                                        
+
+
+# exact sum decision rule
+for sample in n_sample_range:
+    for epsilon in epsilon_range:
+        for n_treatments in n_treatments_range:
+            print(f"sample_size: {sample}, epsilon: {epsilon}, n_treatments: {n_treatments}")
             
+            # let us fix true probabilities
+            theta = np.random.uniform(0,1,n_treatments)
+            best_theta = np.argmax(theta)
+            print("Best treatment is " + str(best_theta))
+            epsilon_opt_thetas = np.where(np.logical_and(theta>=theta[best_theta]-epsilon, theta<=theta[best_theta]+epsilon))
+            print("Epsilon optimal treatments are " + str(epsilon_opt_thetas))
+            
+            # uniform prior
+            alphas = np.ones(n_treatments)
+            betas = np.ones(n_treatments)
+
+            for i in trange(sims): 
+                remaining_treatments = np.arange(n_treatments)
+                j = 1
+                rounds = math.floor(math.log2(n_treatments))
+                if j < rounds:      
+                    outcome_treatment = np.random.binomial(sample, theta[remaining_treatments])
+                    alphas[remaining_treatments] = alphas[remaining_treatments] + outcome_treatment
+                    betas[remaining_treatments] = betas[remaining_treatments] + sample - outcome_treatment
+                
+                    bool_survived_treatments = exact_sum(alphas[remaining_treatments], 
+                                                            betas[remaining_treatments], 
+                                                            epsilon)
+                    
+                    remaining_treatments = remaining_treatments[bool_survived_treatments == 1]
+                if np.in1d(remaining_treatments,epsilon_opt_thetas).any()==1:
+                    pos1 = n_sample_range.index(sample)
+                    pos2 = epsilon_range.index(epsilon)
+                    pos3 = n_treatments_range.index(n_treatments)
+                    counter_es[pos1,pos2,pos3] = counter_es[pos1,pos2,pos3] + 1
+
+# rate of success
+counter_ni/sims
+counter_na/sims
+counter_sa/sims
+counter_es/sims
